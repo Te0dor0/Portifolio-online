0b93810043733ddf6ed2bceb2f53a3972ddc48c2
Te0dor0 <1.62857354e+08+Te0dor0@users.noreply.github.com>
2026-05-25 22:42:58 +0000

    Update base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  base: "/my-digital-showcase-110/",
+  base: "/Portifolio-online/",
   plugins: [
     TanStackRouterVite(),
     tanstackStart({ server: { entry: "server" } }),
