6945e2062c8604fb18ceba6e0ddae267dcd54535
Te0dor0 <1.62857354e+08+Te0dor0@users.noreply.github.com>
2026-05-25 23:13:25 +0000

    Fix Cloudflare Pages build and remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  base: "/Portifolio-online/",
+  
   plugins: [
     TanStackRouterVite(),
     tanstackStart({ server: { entry: "server" } }),
